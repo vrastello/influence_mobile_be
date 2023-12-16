7e449a19ce43315d56dcb43cd54c7784a78790a3
vrastello <vincent.rastello@spin.pm>
2023-12-16 15:05:40 -0800

    adding age to users for simpler queries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_16_230213) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_16_230458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_16_230213) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.integer "age"
   end
 
 end
