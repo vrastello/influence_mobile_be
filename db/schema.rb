d455102630f6958498288feb318a7288bb51d066
vrastello <vincent.rastello@spin.pm>
2023-12-17 11:59:40 -0800

    adding roles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_16_231232) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_17_195858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_16_231232) do
     t.string "email"
     t.string "password_digest"
     t.integer "age"
+    t.string "role"
   end
 
 end
