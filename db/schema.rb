7ca0042e567fbf79943bef7fd0cbf0ebaf943216
vrastello <vincent.rastello@spin.pm>
2023-12-17 12:11:19 -0800

    add column default to users role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_17_200241) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_17_201014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_17_200241) do
     t.string "email"
     t.string "password_digest"
     t.integer "age"
-    t.string "role"
+    t.string "role", default: "user"
   end
 
 end
